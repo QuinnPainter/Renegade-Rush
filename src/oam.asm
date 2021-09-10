85fcaff6fbc7c198599872d25fea43494cce79a9
Quinn Painter <quinn@geekymonkey.com>
2021-09-10 13:36:01 +0100

    fix some OAM DMA stuff

--- src/oam.asm
+++ src/oam.asm
@@ -6,21 +6,21 @@ SpriteBuffer::
 
 SECTION "DMARoutineROM", ROM0
 ; The routine that gets copied into HRAM and executed during the DMA transfer
-; Is 14 bytes long
 ; Sets - A to 0
 DMARoutine::
     ; start DMA transfer
-    ld a, SpriteBuffer >> 8
-    ld [rDMA], a
+    ld a, HIGH(SpriteBuffer) ;c2 b2
+    ldh [rDMA], a ;c3 b2
     ; wait 160 cycles/microseconds, the time it takes for the
     ; transfer to finish; this works because 'dec' is 1 cycle
     ; and 'jr' is 3, for 4 cycles done 40 times
-    ld a, 40
+    ld a, 40 ; c2 b2
 .loop:
-    dec a
-    jr nz, .loop
-    ret
+    dec a ;c1 b1
+    jr nz, .loop ;c3/2 b2
+    ret ;c4 b1
+DMARoutineEnd:
 
 SECTION "DMARoutineHRAM", HRAM
 DMARoutineHRAM::
-    DS 14 ; Reserve 14 bytes in HRAM for the OAM DMA routine
+    DS DMARoutineEnd - DMARoutine ; Reserve space in HRAM for the OAM DMA routine
