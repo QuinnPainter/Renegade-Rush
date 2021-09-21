3285fcb7e1c932e58e482b9d373a77f52182d6d6
Quinn Painter <quinn@geekymonkey.com>
2021-09-21 18:43:31 +0100

    Organise graphics assets

--- assets.mk
+++ assets.mk
@@ -33,70 +33,70 @@ title: $(RESDIR)/title.2bpp $(RESDIR)/title.tilemap $(RESDIR)/titleScreenBottom.
 font: $(RESDIR)/font.2bpp $(RESDIR)/fontPSwap.2bpp
 garage: $(RESDIR)/garage.2bpp $(RESDIR)/garage.tilemap $(RESDIR)/garageObjects.2bpp
 
-$(RESDIR)/lines.2bpp $(RESDIR)/lines.tilemap: $(ASSETSDIR)/lines.png | $(RESDIR)
-	$(GFX) -u -o $(RESDIR)/lines.2bpp -t $(RESDIR)/lines.tilemap $(ASSETSDIR)/lines.png
-$(RESDIR)/lines2.2bpp: $(ASSETSDIR)/lines2.png | $(RESDIR)
-	$(GFX) -u -o $(RESDIR)/lines2.2bpp $(ASSETSDIR)/lines2.png
-$(RESDIR)/lines3.2bpp: $(ASSETSDIR)/lines3.png | $(RESDIR)
-	$(GFX) -u -o $(RESDIR)/lines3.2bpp $(ASSETSDIR)/lines3.png
-$(RESDIR)/lines4.2bpp: $(ASSETSDIR)/lines4.png | $(RESDIR)
-	$(GFX) -u -o $(RESDIR)/lines4.2bpp $(ASSETSDIR)/lines4.png
+$(RESDIR)/lines.2bpp $(RESDIR)/lines.tilemap: $(ASSETSDIR)/road/lines.png | $(RESDIR)
+	$(GFX) -u -o $(RESDIR)/lines.2bpp -t $(RESDIR)/lines.tilemap $(ASSETSDIR)/road/lines.png
+$(RESDIR)/lines2.2bpp: $(ASSETSDIR)/road/lines2.png | $(RESDIR)
+	$(GFX) -u -o $(RESDIR)/lines2.2bpp $(ASSETSDIR)/road/lines2.png
+$(RESDIR)/lines3.2bpp: $(ASSETSDIR)/road/lines3.png | $(RESDIR)
+	$(GFX) -u -o $(RESDIR)/lines3.2bpp $(ASSETSDIR)/road/lines3.png
+$(RESDIR)/lines4.2bpp: $(ASSETSDIR)/road/lines4.png | $(RESDIR)
+	$(GFX) -u -o $(RESDIR)/lines4.2bpp $(ASSETSDIR)/road/lines4.png
 
-$(RESDIR)/starterCar.2bpp: $(ASSETSDIR)/starterCar.png | $(RESDIR)
-	$(GFX) -h -o $(RESDIR)/starterCar.2bpp $(ASSETSDIR)/starterCar.png
-$(RESDIR)/truck.2bpp: $(ASSETSDIR)/truck.png | $(RESDIR)
-	$(GFX) -h -o $(RESDIR)/truck.2bpp $(ASSETSDIR)/truck.png
+$(RESDIR)/starterCar.2bpp: $(ASSETSDIR)/gameobjs/starterCar.png | $(RESDIR)
+	$(GFX) -h -o $(RESDIR)/starterCar.2bpp $(ASSETSDIR)/gameobjs/starterCar.png
+$(RESDIR)/truck.2bpp: $(ASSETSDIR)/gameobjs/truck.png | $(RESDIR)
+	$(GFX) -h -o $(RESDIR)/truck.2bpp $(ASSETSDIR)/gameobjs/truck.png
 
-$(RESDIR)/policecar.2bpp $(RESDIR)/policecar.tilemap: $(ASSETSDIR)/policecar.png | $(RESDIR)
-	$(GFX) -h -o $(RESDIR)/policecar.2bpp $(ASSETSDIR)/policecar.png
+$(RESDIR)/policecar.2bpp $(RESDIR)/policecar.tilemap: $(ASSETSDIR)/gameobjs/policecar.png | $(RESDIR)
+	$(GFX) -h -o $(RESDIR)/policecar.2bpp $(ASSETSDIR)/gameobjs/policecar.png
 	$(ADDOFFSET) $(MAPSDIR)/policecar.tilemap $(RESDIR)/policecar.tilemap 16
 
-$(RESDIR)/explosion1.2bpp: $(ASSETSDIR)/explosion1.png | $(RESDIR)
-	$(GFX) -h -o $(RESDIR)/explosion1.2bpp $(ASSETSDIR)/explosion1.png
+$(RESDIR)/explosion1.2bpp: $(ASSETSDIR)/gameobjs/explosion1.png | $(RESDIR)
+	$(GFX) -h -o $(RESDIR)/explosion1.2bpp $(ASSETSDIR)/gameobjs/explosion1.png
 
 $(RESDIR)/policecarcol.bin: | $(RESDIR)
 	$(CARCOLGEN) $(RESDIR)/policecarcol.bin 2.3
 
-$(RESDIR)/helicopter.2bpp: $(ASSETSDIR)/helicopter.png | $(RESDIR)
-	$(GFX) -h -o $(RESDIR)/helicopter.2bpp $(ASSETSDIR)/helicopter.png
+$(RESDIR)/helicopter.2bpp: $(ASSETSDIR)/gameobjs/helicopter.png | $(RESDIR)
+	$(GFX) -h -o $(RESDIR)/helicopter.2bpp $(ASSETSDIR)/gameobjs/helicopter.png
 
-$(RESDIR)/helicopterExplode.2bpp: $(ASSETSDIR)/helicopterExplode.png | $(RESDIR)
-	$(GFX) -h -o $(RESDIR)/helicopterExplode.2bpp $(ASSETSDIR)/helicopterExplode.png
+$(RESDIR)/helicopterExplode.2bpp: $(ASSETSDIR)/gameobjs/helicopterExplode.png | $(RESDIR)
+	$(GFX) -h -o $(RESDIR)/helicopterExplode.2bpp $(ASSETSDIR)/gameobjs/helicopterExplode.png
 
-$(RESDIR)/missile.2bpp: $(ASSETSDIR)/missile.png | $(RESDIR)
-	$(GFX) -h -o $(RESDIR)/missile.2bpp $(ASSETSDIR)/missile.png
+$(RESDIR)/missile.2bpp: $(ASSETSDIR)/gameobjs/missile.png | $(RESDIR)
+	$(GFX) -h -o $(RESDIR)/missile.2bpp $(ASSETSDIR)/gameobjs/missile.png
 
 $(RESDIR)/roadcollision.bin: $(RESDIR)/lines.tilemap | $(RESDIR)
 	$(ROADCOLGEN) $(RESDIR)/roadcollision.bin $(RESDIR)/lines.tilemap
 
-$(RESDIR)/statusbar.2bpp: $(ASSETSDIR)/statusbar.png | $(RESDIR)
-	$(GFX) -x 3 -o $(RESDIR)/statusbar.2bpp $(ASSETSDIR)/statusbar.png
+$(RESDIR)/statusbar.2bpp: $(ASSETSDIR)/interface/statusbar.png | $(RESDIR)
+	$(GFX) -x 3 -o $(RESDIR)/statusbar.2bpp $(ASSETSDIR)/interface/statusbar.png
 
 $(RESDIR)/curvebar.tilemap: | $(RESDIR)
 	$(CURVEBARGEN) $(RESDIR)/curvebar.tilemap
 
-$(RESDIR)/menubar.2bpp $(RESDIR)/menubar.tilemap: $(ASSETSDIR)/menubar.png | $(RESDIR)
-	$(GFX) -u -o $(RESDIR)/menubar.2bpp -t $(RESDIR)/menubar.tilemap $(ASSETSDIR)/menubar.png
+$(RESDIR)/menubar.2bpp $(RESDIR)/menubar.tilemap: $(ASSETSDIR)/interface/menubar.png | $(RESDIR)
+	$(GFX) -u -o $(RESDIR)/menubar.2bpp -t $(RESDIR)/menubar.tilemap $(ASSETSDIR)/interface/menubar.png
 
-$(RESDIR)/title.2bpp $(RESDIR)/title.tilemap: $(ASSETSDIR)/title.png | $(RESDIR)
-	$(GFX) -u -o $(RESDIR)/title.2bpp -t $(RESDIR)/title.tilemap $(ASSETSDIR)/title.png
+$(RESDIR)/title.2bpp $(RESDIR)/title.tilemap: $(ASSETSDIR)/interface/title.png | $(RESDIR)
+	$(GFX) -u -o $(RESDIR)/title.2bpp -t $(RESDIR)/title.tilemap $(ASSETSDIR)/interface/title.png
 	$(ADDOFFSET) $(RESDIR)/title.tilemap $(RESDIR)/title.tilemap 23
 
-$(RESDIR)/titleScreenBottom.2bpp $(RESDIR)/titleScreenBottom.tilemap: $(ASSETSDIR)/titleScreenBottom.png | $(RESDIR)
-	$(GFX) -u -o $(RESDIR)/titleScreenBottom.2bpp -t $(RESDIR)/titleScreenBottom.tilemap $(ASSETSDIR)/titleScreenBottom.png
+$(RESDIR)/titleScreenBottom.2bpp $(RESDIR)/titleScreenBottom.tilemap: $(ASSETSDIR)/interface/titleScreenBottom.png | $(RESDIR)
+	$(GFX) -u -o $(RESDIR)/titleScreenBottom.2bpp -t $(RESDIR)/titleScreenBottom.tilemap $(ASSETSDIR)/interface/titleScreenBottom.png
 	$(ADDOFFSET) $(RESDIR)/titleScreenBottom.tilemap $(RESDIR)/titleScreenBottom.tilemap -52
 
-$(RESDIR)/garage.2bpp $(RESDIR)/garage.tilemap: $(ASSETSDIR)/garage.png | $(RESDIR)
-	$(GFX) -u -o $(RESDIR)/garage.2bpp -t $(RESDIR)/garage.tilemap $(ASSETSDIR)/garage.png
+$(RESDIR)/garage.2bpp $(RESDIR)/garage.tilemap: $(ASSETSDIR)/interface/garage.png | $(RESDIR)
+	$(GFX) -u -o $(RESDIR)/garage.2bpp -t $(RESDIR)/garage.tilemap $(ASSETSDIR)/interface/garage.png
 
-$(RESDIR)/garageObjects.2bpp: $(ASSETSDIR)/garageObjects.png | $(RESDIR)
-	$(GFX) -u -o $(RESDIR)/garageObjects.2bpp $(ASSETSDIR)/garageObjects.png
+$(RESDIR)/garageObjects.2bpp: $(ASSETSDIR)/interface/garageObjects.png | $(RESDIR)
+	$(GFX) -u -o $(RESDIR)/garageObjects.2bpp $(ASSETSDIR)/interface/garageObjects.png
 
-$(RESDIR)/font.2bpp: $(ASSETSDIR)/font.png | $(RESDIR)
-	$(GFX) -x 11 -o $(RESDIR)/font.2bpp $(ASSETSDIR)/font.png
+$(RESDIR)/font.2bpp: $(ASSETSDIR)/interface/font.png | $(RESDIR)
+	$(GFX) -x 11 -o $(RESDIR)/font.2bpp $(ASSETSDIR)/interface/font.png
 
-$(RESDIR)/fontPSwap.2bpp: $(ASSETSDIR)/fontPSwap.png | $(RESDIR)
-	$(GFX) -x 6 -o $(RESDIR)/fontPSwap.2bpp $(ASSETSDIR)/fontPSwap.png
+$(RESDIR)/fontPSwap.2bpp: $(ASSETSDIR)/interface/fontPSwap.png | $(RESDIR)
+	$(GFX) -x 6 -o $(RESDIR)/fontPSwap.2bpp $(ASSETSDIR)/interface/fontPSwap.png
 
 $(RESDIR):
 	$(MKDIR_P) $(RESDIR)
