63d23856dbc2538a118583148c7cde801c63ba45
Quinn Painter <quinn@geekymonkey.com>
2021-09-26 17:52:50 +0100

    fix charge bar bug

--- scripts/GenCurvedBarTilemap.py
+++ scripts/GenCurvedBarTilemap.py
@@ -31,7 +31,7 @@ tilemaps = [TL_OFF, TR_TOFF_BOFF, BL_LOFF_ROFF, BR_LOFF_TOFF, # 0 = 0000 - all b
             TL_OFF, TR_TOFF_BOFF, BL_LOFF_RON, BR_LON_TOFF, # 4 = 0100 - special stage 1
             TL_ON, TR_TOFF_BOFF, BL_LON_RON, BR_LON_TOFF, # 5 = 0101 - special stage 1 & missile stage 1
             TL_OFF, TR_TON_BOFF, BL_LOFF_RON, BR_LON_TOFF, # 6 = 0110 - special stage 1 & missile stage 2
-            TL_ON, TR_TON_BOFF, BL_LON_ROFF, BR_LON_TOFF, # 7 = 0111 - special stage 1 & missile stage 2 & 3
+            TL_ON, TR_TON_BOFF, BL_LON_RON, BR_LON_TOFF, # 7 = 0111 - special stage 1 & missile stage 1 & 2
             TL_OFF, TR_TOFF_BON, BL_LOFF_ROFF, BR_LOFF_TON, # 8 = 1000 - special stage 2
             TL_ON, TR_TOFF_BON, BL_LON_ROFF, BR_LOFF_TON, # 9 = 1001 - special stage 2 & missile stage 1
             TL_OFF, TR_TON_BON, BL_LOFF_ROFF, BR_LOFF_TON, # 10 = 1010 - special stage 2 & missile stage 2
